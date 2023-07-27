0baee2add4c0eb0a187a6996447cda0d17e4e058
francinemassaro <francine.massaro@gmail.com>
2023-07-27 19:31:43 -0300

    changing the amount of data per page to 10, and sorting alphabetically from name

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -8,6 +8,7 @@ import med.voll.api.repository.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,7 +28,7 @@ public class DoctorController {
     }
 
     @GetMapping
-    public Page<DataDoctorsListDTO> getAll(Pageable pagination) {
+    public Page<DataDoctorsListDTO> getAll(@PageableDefault(size = 10, sort = {"name"}) Pageable pagination) {
         return repository.findAll(pagination).map(DataDoctorsListDTO::new);
     }
 }
