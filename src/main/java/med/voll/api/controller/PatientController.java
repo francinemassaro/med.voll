e0e43ec7dec6c4dced9b87031efc8b434d19fda2
francinemassaro <francine.massaro@gmail.com>
2023-07-28 17:25:30 -0300

    add getAll of patients

--- src/main/java/med/voll/api/controller/PatientController.java
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -1,12 +1,15 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.controller.dto.DataPatientsListDTO;
 import med.voll.api.controller.dto.PatientRecordDataDTO;
 import med.voll.api.domain.Patient;
 import med.voll.api.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/patients")
@@ -21,4 +24,8 @@ public class PatientController {
         repository.save(new Patient(data));
     }
 
+    @GetMapping
+    public List<DataPatientsListDTO> getAll(){
+       return repository.findAll().stream().map(DataPatientsListDTO::new).toList();
+    }
 }
