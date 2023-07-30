14798401d0ec244cee32a7b0b3a7ef1f906c1b75
francinemassaro <francine.massaro@gmail.com>
2023-07-30 17:02:57 -0300

    updating list of patients to active only.

--- src/main/java/med/voll/api/controller/PatientController.java
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -7,10 +7,11 @@ import med.voll.api.controller.dto.PatientUpdateData;
 import med.voll.api.domain.Patient;
 import med.voll.api.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/patients")
@@ -21,25 +22,25 @@ public class PatientController {
 
     @PostMapping
     @Transactional
-    public void create(@RequestBody @Valid PatientRecordDataDTO data){
+    public void create(@RequestBody @Valid PatientRecordDataDTO data) {
         repository.save(new Patient(data));
     }
 
     @GetMapping
-    public List<DataPatientsListDTO> getAll(){
-       return repository.findAll().stream().map(DataPatientsListDTO::new).toList();
+    public Page<DataPatientsListDTO> getAll(@PageableDefault(size = 10, sort = {"name"}) Pageable pagination) {
+        return repository.findAllByActiveTrue(pagination).map(DataPatientsListDTO::new);
     }
 
     @PutMapping
     @Transactional
-    public void update(@RequestBody @Valid PatientUpdateData data){
+    public void update(@RequestBody @Valid PatientUpdateData data) {
         var patient = repository.getReferenceById(data.id());
         patient.updateInfo(data);
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void delete(@PathVariable Long id){
+    public void delete(@PathVariable Long id) {
         var patient = repository.getReferenceById(id);
         patient.delete();
     }
--- src/main/java/med/voll/api/repository/PatientRepository.java
+++ src/main/java/med/voll/api/repository/PatientRepository.java
@@ -1,7 +1,12 @@
 package med.voll.api.repository;
 
 import med.voll.api.domain.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findAllByActiveTrue(Pageable pagination);
 }
